9146794049cc974c89112040a94c769cf655a8e5
smguy9 <yagizcan.arslan93@gmail.com>
2021-04-10 22:41:11 +0300

    DeviceController

---
+++ app/Http/Controllers/DeviceController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Device;
+use Illuminate\Http\Request;
+
+class DeviceController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Device $device)
+    {
+        //
+    }
+
+    public function update(Request $request, Device $device)
+    {
+        //
+    }
+
+    public function destroy(Device $device)
+    {
+        //
+    }
+}
