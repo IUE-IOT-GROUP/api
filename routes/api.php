7872907d80b36c40a1ae36271d334d663c343f93
smguy9 <yagizcan.arslan93@gmail.com>
2021-04-10 23:07:50 +0300

    login

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::group(['prefix' => 'v1'], function() {
-    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'login'])->name('api.login');
 
     Route::group(['middleware' => 'auth:sanctum'], function() {
         Route::get('me', [ProfileController::class, 'index']);
