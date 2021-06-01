f1135bb4e7e96b29c87668fd459bf13598e6ddd0
smguy9 <yagizcan.arslan93@gmail.com>
2021-06-01 04:29:28 +0300

    add error and success methods to base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,14 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function error($message = 'An error has occurred.', $code = 404): \Illuminate\Http\JsonResponse
+    {
+        return response()->json(['success' => false, 'message' => $message], $code);
+    }
+
+    public function success($message = 'Success', $code = 200): \Illuminate\Http\JsonResponse
+    {
+        return response()->json(['success' => true, 'message' => $message], $code);
+    }
 }
