cbf9ba8969eaefadc389c37ee65d017314811f78
smguy9 <yagizcan.arslan93@gmail.com>
2021-06-01 04:29:00 +0300

    added auth request

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Auth\StoreRequest;
 use App\Models\User;
 use Exception;
-use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
@@ -12,32 +12,31 @@ class AuthController extends Controller
     /**
      * @throws ValidationException
      */
-    public function login(Request $request): \Illuminate\Http\JsonResponse
+    public function login(StoreRequest $request): \Illuminate\Http\JsonResponse
     {
-        $request->validate([
-            'email' => 'required',
-            'password' => 'required',
-            'device_name' => 'required'
-        ]);
 
-        try {
+        try
+        {
             $user = User::where('email', $request->email)->firstOrFail();
-        } catch (Exception $e) {
+        } catch (Exception $e)
+        {
             throw ValidationException::withMessages([
-                'email' => 'The given email does not exists.'
+                'email' => 'The given email does not exists.',
             ]);
         }
 
-        if (!\Hash::check($request->password, $user->password)) {
+        if (!\Hash::check($request->password, $user->password))
+        {
             throw ValidationException::withMessages([
-                'email' => 'The provided credentials are incorrect.'
+                'email' => 'The provided credentials are incorrect.',
             ]);
         }
 
         return response()->json([
+            'id' => $user->id,
             'username' => $user->name,
             'email' => $user->email,
-            'token' => $user->createToken($request->device_name)->plainTextToken
+            'token' => $user->createToken($request->device_name)->plainTextToken,
         ]);
     }
 }
---
+++ app/Http/Requests/Auth/StoreRequest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    public function rules()
+    {
+        return [
+            'email' => 'required',
+            'password' => 'required',
+            'device_name' => 'required'
+        ];
+    }
+}
