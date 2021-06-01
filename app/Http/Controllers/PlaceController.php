fcca6834f17818666d40b1b0829a02cd4e8428dc
smguy9 <yagizcan.arslan93@gmail.com>
2021-06-01 04:31:02 +0300

    update places

--- app/Http/Controllers/PlaceController.php
+++ app/Http/Controllers/PlaceController.php
@@ -2,17 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\Place\PlaceCollection;
 use App\Http\Resources\Place\PlaceResource;
 use App\Models\Place;
 use Illuminate\Http\Request;
 
 class PlaceController extends Controller
 {
-    public function index(Request $request)
+    public function __construct()
     {
-        return new PlaceCollection(
-            $request->user()->places()->whereParentId(null)->get()
+        $this->middleware(function ($request, $next) {
+            $place = $request->route()->parameters('place');
+
+            if (!empty($place))
+            {
+                $exists = $request->user()->places->contains($place['place']);
+
+                if (!$exists)
+                {
+                    return $this->error('Place not found');
+                }
+            }
+
+            return $next($request);
+        });
+    }
+
+    public function index(Request $request): \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+    {
+        return PlaceResource::collection(
+            $request->user()->places()->whereParentId(null)->with('children')->get()
         );
     }
 
@@ -20,27 +38,27 @@ class PlaceController extends Controller
     {
         $request->validate([
             'name' => 'required',
-            'parent' => ['nullable', 'exists:places,id']
+            'parent' => ['nullable', 'exists:places,id'],
         ]);
 
         if ($request->parent)
         {
             $parentPlace = Place::findOrFail($request->parent);
             $place = $parentPlace->children()->create([
-                'name' => $request->name
+                'name' => $request->name,
             ]);
         }
         else
         {
             $place = $request->user()->places()->create([
-                'name' => $request->name
+                'name' => $request->name,
             ]);
         }
 
         return new PlaceResource($place);
     }
 
-    public function show(Place $place)
+    public function show(Request $request, Place $place)
     {
         return new PlaceResource($place);
     }
@@ -49,7 +67,7 @@ class PlaceController extends Controller
     {
         $request->validate([
             'name' => 'required',
-            'parent' => ['nullable', 'exists:places,id']
+            'parent' => ['nullable', 'exists:places,id'],
         ]);
 
         if ($request->parent)
@@ -57,13 +75,13 @@ class PlaceController extends Controller
             $parentPlace = Place::findOrFail($request->parent);
             $place = $place->update([
                 'name' => $request->name,
-                'parent_id' => $parentPlace->id
+                'parent_id' => $parentPlace->id,
             ]);
         }
         else
         {
             $place->update([
-                'name' => $request->name
+                'name' => $request->name,
             ]);
         }
 
@@ -76,6 +94,7 @@ class PlaceController extends Controller
         $place->devices()->delete();
         $place->delete();
 
-        return response()->json(['success' => true]);
+
+        return $this->success();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,33 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DataController;
+use App\Http\Controllers\DeviceController;
 use App\Http\Controllers\PlaceController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserDeviceController;
+use App\Http\Controllers\UserDevicePlaceController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'login'])->name('api.login');
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('me', [ProfileController::class, 'index']);
-    Route::apiResource('users', UserController::class);
-    Route::apiResource('places', PlaceController::class);
+    Route::post('devices/data', [DataController::class, 'store']);
+    Route::apiResource('userDevices', UserDeviceController::class)->missing(function () {
+        return error('Place not found');
+    });;
+    Route::apiResource('devices', DeviceController::class)->missing(function () {
+        return error('Place not found');
+    });;
+    Route::apiResource('users', UserController::class)->missing(function () {
+        return error('Place not found');
+    });;
+    Route::apiResource('places', PlaceController::class)->missing(function () {
+        return error('Place not found');
+    });
+    Route::apiResource('places.userDevices', UserDevicePlaceController::class)->missing(function () {
+        return error('Place not found');
+    });
 });
