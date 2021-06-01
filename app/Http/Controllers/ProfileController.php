4c6de5f38412066769e0f6764bdf24c17362a05a
smguy9 <yagizcan.arslan93@gmail.com>
2021-06-01 04:35:21 +0300

    user update logic

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\User\UserResource;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
 
 class ProfileController extends Controller
 {
@@ -11,13 +12,7 @@ class ProfileController extends Controller
     {
         return new UserResource($request->user());
     }
-
-    public function store(Request $request)
-    {
-
-    }
-
-
+/*
     public function show($id)
     {
         //
@@ -25,11 +20,22 @@ class ProfileController extends Controller
 
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'name' => ['required'],
+            'email' => ['required'],
+            'phone_number' => ['nullable'],
+            'password' => ['nullable', 'confirmed', Password::min(4)->numbers()]
+        ]);
+
+        $request->user()->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone_number' => $request->phone_number,
+        ]);
     }
 
     public function destroy($id)
     {
         //
-    }
+    }*/
 }
