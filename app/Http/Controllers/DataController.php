507f44033b74aaaf60c40c2c316c98f9811de251
smguy9 <yagizcan.arslan93@gmail.com>
2021-06-13 01:35:56 +0300

    Update period of device data

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -108,31 +108,48 @@ class DataController extends Controller
                 {
                     case 'daily':
                         $period = Carbon::now()->subDays();
-                        $query = $query->where('created_at', '>=', $period)
-                            ->groupByRaw('HOUR(created_at)');
+                        $query = $query->where('created_at', '>=', $period);
                         break;
                     case 'weekly':
                         $period = Carbon::now()->subDays(6);
-                        $query = $query->where('created_at', '>', $period)
-                            ->groupByRaw('DAY(created_at)');
+                        $query = $query->where('created_at', '>', $period);
                         break;
                     case 'monthly':
                         $period = Carbon::now()->subDays(30);
-                        $query = $query->where('created_at', '>=', $period)
-                            ->groupByRaw('WEEK(created_at)');
+                        $query = $query->where('created_at', '>=', $period);
                         break;
                     default:
                         $period = Carbon::now()->subDays();
-                        $query = $query->where('created_at', '>=', $period)
-                            ->groupByRaw('HOUR(created_at)');
+                        $query = $query->where('created_at', '>=', $period);
                 }
 
                 return $query;
             })
-            ->select([\DB::raw('AVG(value) AS value'), 'user_device_id', 'device_parameter_id', 'created_at'])
             ->orderByDesc('created_at');
 
-        $collectedData = DeviceDataResource::collection($query->get());
+        $collectedData = DeviceDataResource::collection($query->take(10)->get());
+
+        $graphData = DeviceDataResource::collection(
+            $query->when($request->get('period'), function ($query, $value) {
+                switch ($value)
+                {
+                    case 'daily':
+                        $query = $query->groupByRaw('HOUR(created_at)');
+                        break;
+                    case 'weekly':
+                        $query = $query->groupByRaw('DAY(created_at)');
+                        break;
+                    case 'monthly':
+                        $query = $query->groupByRaw('WEEK(created_at)');
+                        break;
+                    default:
+                        $query = $query->groupByRaw('HOUR(created_at)');
+                }
+
+                return $query;
+            })->select([\DB::raw('AVG(value) AS value'), 'user_device_id', 'device_parameter_id', 'created_at'])
+                ->get()
+        );
 
         $data['data'] = [
             'details' => [
@@ -141,13 +158,13 @@ class DataController extends Controller
                 'unit' => $type->parameter->unit,
                 'expected_parameter' => $type->expected_parameter,
             ],
-            'min_y' => $collectedData->min('value'),
-            'max_y' => $collectedData->max('value'),
-            'min_x' => $collectedData->min('created_at'),
-            'max_x' => $collectedData->max('created_at'),
-            'count' => $collectedData->count(),
-            'data' => $collectedData->take(10),
-            'graphData' => $collectedData,
+            'min_y' => $graphData->min('value'),
+            'max_y' => $graphData->max('value'),
+            'min_x' => $graphData->min('created_at'),
+            'max_x' => $graphData->max('created_at'),
+            'count' => $graphData->count(),
+            'data' => $collectedData,
+            'graphData' => $graphData,
         ];
 
         return response()->json($data);
