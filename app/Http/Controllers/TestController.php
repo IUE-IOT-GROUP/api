f4238c7d2b400f19bc2a325dcb97405286b2641a
smguy9 <yagizcan.arslan93@gmail.com>
2021-06-13 02:39:21 +0300

    Update TestController.php

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -15,8 +15,8 @@ class TestController extends Controller
 {
     public function add_device_data()
     {
-        $since = Carbon::create(2021, 6, 12, 14, 00, 00);
-        $until = Carbon::now();
+        $since = Carbon::now()->subMonths();
+        $until = Carbon::now()->addHours(3);
 
         $interval = collect(CarbonPeriod::since($since)->minutes(5)->until($until));
         $faker = Factory::create();
