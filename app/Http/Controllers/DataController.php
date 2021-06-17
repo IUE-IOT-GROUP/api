438f5f17723894f91916962ae4ab2eff64ab521c
smguy9 <yagizcan.arslan93@gmail.com>
2021-06-17 05:08:06 +0300

    prevent errors when data is older than one day

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -23,16 +23,14 @@ class DataController extends Controller
             'mac_address' => ['required', 'regex:/^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/'],
         ]);
 
-        if (filled($request->mac_address))
-        {
+        if (filled($request->mac_address)) {
             // get the device with the given mac address
             $devices = UserDevice::whereMacAddress($request->mac_address)->get();
 
             // there might be multiple devices with the same mac address
             $devices->each(function (UserDevice $device) use ($request) {
                 // look for the parameters except the mac address
-                foreach ($request->except('mac_address') as $parameter => $value)
-                {
+                foreach ($request->except('mac_address') as $parameter => $value) {
                     // find the user parameters related to device with the expected parameter
                     $device_parameters = $device->parameters()->wherePivot('expected_parameter', $parameter);
 
@@ -40,8 +38,7 @@ class DataController extends Controller
                     $device_parameters = $device_parameters->get()->first();
 
                     // if parameters are found process them
-                    if (!is_null($device_parameters))
-                    {
+                    if (!is_null($device_parameters)) {
                         // we need the id of the pivot table
                         $device_parameters = $device_parameters->parameters;
 
@@ -63,35 +60,50 @@ class DataController extends Controller
     public function show(UserDevice $device): \Illuminate\Http\JsonResponse
     {
         $data = [];
+
         $device->parameters->each(function (ParameterType $type) use (&$data) {
-            $query = DeviceData::query()
-                ->where('device_parameter_id', $type->parameters->id)
-                ->where('created_at', '>=', Carbon::now()->subDays())
-                ->orderByDesc('created_at');
+            $latest = DeviceData::where('device_parameter_id', $type->parameters->id)->take(1)->latest()->first();
 
-            $collectedData = DeviceDataResource::collection($query->get());
-
-            $graphQuery = $query->groupByRaw('date_format(created_at, \'%Y%m%d%H\')')
-                ->select([\DB::raw('AVG(value) AS value'), 'user_device_id', 'device_parameter_id', \DB::raw('MIN(created_at) as created_at')])
-                ->get();
-
-            $graphData = DeviceDataResource::collection($graphQuery);
-
-            $data['data'][] = [
-                'details' => [
-                    'device_parameter_id' => $type->parameters->id,
-                    'name' => $type->name,
-                    'unit' => $type->unit,
-                    'expected_parameter' => $type->parameters->expected_parameter,
-                ],
-                'min_y' => $graphData->min('value'),
-                'max_y' => $graphData->max('value'),
-                'min_x' => $graphQuery->min('created_at')->format('Y-m-d H:i:s'),
-                'max_x' => $collectedData->max('created_at')->format('Y-m-d H:i:s'),
-                'count' => $collectedData->count(),
-                'data' => $collectedData->take(10),
-                'graphData' => $graphData,
+            $details['details'] = [
+                'device_parameter_id' => $type->parameters->id,
+                'name' => $type->name,
+                'unit' => $type->unit,
+                'expected_parameter' => $type->parameters->expected_parameter,
             ];
+            $returningGraphData = [];
+
+            if (!is_null($latest)) {
+                $yesterday = Carbon::now();
+
+                // if the latest data is before yesterday, load graph data by the latest data
+                if ($latest->created_at->diff($yesterday)->days >= 1)
+                    $yesterday = $latest->created_at;
+
+                $query = DeviceData::query()
+                    ->where('device_parameter_id', $type->parameters->id)
+                    ->where('created_at', '>=', $yesterday->subDays())
+                    ->orderByDesc('created_at');
+
+                $collectedData = DeviceDataResource::collection($query->get());
+
+                $graphQuery = $query->groupByRaw('date_format(created_at, \'%Y%m%d%H\')')
+                    ->select([\DB::raw('AVG(value) AS value'), 'user_device_id', 'device_parameter_id', \DB::raw('MIN(created_at) as created_at')])
+                    ->get();
+
+                $graphData = DeviceDataResource::collection($graphQuery);
+
+                $returningGraphData = [
+                    'min_y' => $graphData->min('value'),
+                    'max_y' => $graphData->max('value'),
+                    'min_x' => $graphQuery->min('created_at')->format('Y-m-d H:i:s'),
+                    'max_x' => $collectedData->max('created_at')->format('Y-m-d H:i:s'),
+                    'count' => $collectedData->count(),
+                    'data' => $collectedData->take(10),
+                    'graphData' => $graphData,
+                ];
+            }
+
+            $data['data'][] = array_merge($details, $returningGraphData);
         });
 
         return response()->json($data);
@@ -99,53 +111,69 @@ class DataController extends Controller
 
     public function showParameter(Request $request, UserDevice $device, DeviceParameter $type): \Illuminate\Http\JsonResponse
     {
+        $period = $request->input('period', 'daily');
+        ray()->clearScreen();
         $data = [];
-        $query = DeviceData::query()
-            ->where('device_parameter_id', $type->id)
-            ->where('user_device_id', $device->id)
-            ->when($request->get('period'), function ($query, $value) {
-                return match ($value)
-                {
-                    'weekly' => $query->where('created_at', '>', Carbon::now()->subDays(7)),
-                    'monthly' => $query->where('created_at', '>=', Carbon::now()->subMonths()),
-                    default => $query->where('created_at', '>=', Carbon::now()->subDays()),
+        $latest = DeviceData::where('device_parameter_id', $type->id)->take(1)->latest()->first();
+
+        $details['details'] = [
+            'device_parameter_id' => $type->id,
+            'name' => $type->parameter->name,
+            'unit' => $type->parameter->unit,
+            'expected_parameter' => $type->expected_parameter,
+        ];
+        $returningGraphData = [];
+
+        if (!is_null($latest)) {
+            $yesterday = Carbon::now();
+
+            // if the latest data is before yesterday, load graph data by the latest data
+            if ($latest->created_at->diff($yesterday)->days >= 1)
+                $yesterday = $latest->created_at;
+
+            $query = DeviceData::query()
+                ->where('device_parameter_id', $type->id)
+                ->where('user_device_id', $device->id)
+                ->when($period, function ($query, $value) use ($yesterday) {
+                    return match ($value) {
+                        'weekly' => $query->where('created_at', '>', $yesterday->subDays(7)),
+                        'monthly' => $query->where('created_at', '>=', $yesterday->subMonths()),
+                        default => $query->where('created_at', '>=', $yesterday->subDays()),
+                    };
+                })
+                ->orderByDesc('created_at');
+
+            $collectedData = DeviceDataResource::collection($query->get());
+
+            $graphQuery = $query->when($period, function ($query, $value) {
+                return match ($value) {
+                    'weekly' => $query->groupByRaw('date_format(created_at, \'%Y%m%d\')'),
+                    'monthly' => $query->groupByRaw('date_format(created_at, \'%u\')'),
+                    default => $query->groupByRaw('date_format(created_at, \'%Y%m%d%H\')'),
                 };
             })
-            ->orderByDesc('created_at');
+                ->select([\DB::raw('MAX(id) AS id, AVG(value) AS value'), 'user_device_id', 'device_parameter_id', \DB::raw('MIN(created_at) as created_at')])
+                ->tap(function ($query) {
+                    ray(\Str::replaceArray('?', $query->getBindings(), $query->toSql()))->blue();
+                })
+                ->get();
 
-        $collectedData = DeviceDataResource::collection($query->get());
+            ray('graph', $graphQuery)->blue();
 
-        $graphQuery = $query->when($request->get('period'), function ($query, $value) {
-            return match ($value)
-            {
-                'weekly' => $query->groupByRaw('date_format(created_at, \'%Y%m%d\')'),
-                'monthly' => $query->groupByRaw('date_format(created_at, \'%u\')'),
-                default => $query->groupByRaw('date_format(created_at, \'%Y%m%d%H\')'),
-            };
-        })
-            ->select([\DB::raw('MAX(id) AS id, AVG(value) AS value'), 'user_device_id', 'device_parameter_id', \DB::raw('MIN(created_at) as created_at')])
-            ->tap(function ($query) {
-//                ray(\Str::replaceArray('?', $query->getBindings(), $query->toSql()));
-            })
-            ->get();
+            $graphData = DeviceDataResource::collection($graphQuery);
 
-        $graphData = DeviceDataResource::collection($graphQuery);
+            $returningGraphData = [
+                'min_y' => $graphData->min('value'),
+                'max_y' => $graphData->max('value'),
+                'min_x' => $graphQuery->min('created_at')->format('Y-m-d H:i:s'),
+                'max_x' => $collectedData->max('created_at')->format('Y-m-d H:i:s'),
+                'count' => $graphData->count(),
+                'data' => $collectedData->take(10),
+                'graphData' => $graphData,
+            ];
+        }
 
-        $data['data'] = [
-            'details' => [
-                'device_parameter_id' => $type->id,
-                'name' => $type->parameter->name,
-                'unit' => $type->parameter->unit,
-                'expected_parameter' => $type->expected_parameter,
-            ],
-            'min_y' => $graphData->min('value'),
-            'max_y' => $graphData->max('value'),
-            'min_x' => $graphQuery->min('created_at')->format('Y-m-d H:i:s'),
-            'max_x' => $collectedData->max('created_at')->format('Y-m-d H:i:s'),
-            'count' => $graphData->count(),
-            'data' => $collectedData->take(10),
-            'graphData' => $graphData,
-        ];
+        $data['data'] = array_merge($details, $returningGraphData);
 
         return response()->json($data);
     }
