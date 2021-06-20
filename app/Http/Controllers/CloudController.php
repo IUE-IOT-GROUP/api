2982614f56a2f3fd754b9a9cb96376d665b1c2ad
smguy9 <yagizcan.arslan93@gmail.com>
2021-06-21 00:24:59 +0300

    hotfix

--- app/Http/Controllers/CloudController.php
+++ app/Http/Controllers/CloudController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\DeviceData;
+use Illuminate\Http\Request;
 
 class CloudController extends Controller
 {
-    public function __invoke(\Request $request)
+    public function __invoke(Request $request)
     {
         foreach ($request->get('data') as $data)
         {
